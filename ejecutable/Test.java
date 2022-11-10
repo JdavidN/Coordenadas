e163657f8cbd5c05f6bbdf650a38ed7ad019a07f
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-11-10 08:51:53 -0500

    Add files via upload

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -1,1 +1,44 @@
+package ejecutable;
 
+import modelo.Coordenada;
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        
+        Coordenada C1 = new Coordenada ();
+        C1.setX(0);
+        C1.setY(0);
+
+        Coordenada C2 = new Coordenada(0, 0); 
+
+        //Imprimir Coordenada1
+        System.out.println("Posicion en X: " + C1.getX());
+        System.out.println("Posicion en Y: " + C1.getY());
+        System.out.println(C1.toString());
+        System.out.println("---------------------------------");
+        
+        //Imprimir Coordenada2
+        System.out.println("Posicion en X: " + C2.getX());
+        System.out.println("Posicion en Y: " + C2.getY());
+        System.out.println(C2.toString());
+
+        
+
+        if(C1.equals(C2))
+        {
+            
+            System.out.println("Las Coordenada son iguales");
+        }
+        else
+        {
+            System.out.println("Las Coordenadas son Diferentes");
+        }
+
+       
+        
+        
+        
+    }
+}
